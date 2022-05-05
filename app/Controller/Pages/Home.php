5dada1ca46e2121a7e0ae148fb5f88dedade7203
Eduardo alvez do nascimento <eduardo@manole.local>
2022-05-05 14:57:05 -0300

    No commit anterior foram feitos os controllers nesse foi feito os models

--- app/Controller/Pages/Home.php
+++ app/Controller/Pages/Home.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller\Pages;
 use \App\Utils\View;
+use \App\Model\Entity\OrganizationModel;
 
 class Home extends Page{
     // this method returns the view of home page
     // @return string;
 
     public static function getHome(){
+        $obOrganization = new OrganizationModel;
+
         //home's view
         $content = View::render('pages/home',[
-            'name' => ' DEV - TESTE',
-            'description' => 'Modelo MVC em php puro',
-            'site' => 'https://www.youtube.com/watch?v=TmeyoTNu748'
+            'name' => $obOrganization->name,
+            'description' => $obOrganization->description,
+            'site' => $obOrganization->site
         ]);
         // return page's view
         return parent::getPage('DEV - TESTE - HOME', $content);
