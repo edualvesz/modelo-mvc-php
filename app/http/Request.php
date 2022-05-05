2c7355a1e474a84d25babc60cb8a4fa8f646baee
Eduardo alvez do nascimento <eduardo@manole.local>
2022-05-05 16:53:27 -0300

    Criacao da request

---
+++ app/http/Request.php
@@ -0,0 +1,16 @@
+<?php 
+
+namespace App\Http;
+
+class Request 
+{
+    //http request method
+    //@var string
+    private $httpMethod;
+
+    //page's URI (route)
+    //@var string
+    private $uri;
+
+
+}
