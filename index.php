2f916c8184150f5c78b6ff599bbe83a8c4128a4f
Eduardo alvez do nascimento <eduardo@manole.local>
2022-05-09 15:40:03 -0300

    Criacao da request e da response

--- index.php
+++ index.php
@@ -5,4 +5,10 @@ require __DIR__ . '/vendor/autoload.php';
 //use \App\Controller\Pages\Home;
 use App\Controller\Pages\HomeController;
 
+$obResponse = new \App\Http\Response(200, 'Ola mundo');
+
+$obResponse->sendResponse();
+
+exit;
+
 echo HomeController::getHome();
